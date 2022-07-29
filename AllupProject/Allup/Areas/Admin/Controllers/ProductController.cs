2b950e03e6151080c3f9dbc2996780363ac70042
muradav <muradav@code.edu.az>
2022-07-29 20:14:14 +0400

    feat:product index done

---
+++ AllupProject/Allup/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Allup.DAL;
+using Allup.Extentions;
+using Allup.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Allup.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public ProductController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = _context.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.Category)
+                .Include(p => p.Brand)
+                .ToList();
+            return View(products);
+        }
+
+        
+    }
+}
