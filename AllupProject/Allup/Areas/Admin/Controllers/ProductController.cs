1dfe87b87a4d0731044a42526bcb21b2060f75b4
muradav <muradav@code.edu.az>
2022-07-29 20:58:53 +0400

    feat: product update is written

--- AllupProject/Allup/Areas/Admin/Controllers/ProductController.cs
+++ AllupProject/Allup/Areas/Admin/Controllers/ProductController.cs
@@ -33,6 +33,159 @@ namespace Allup.Areas.Admin.Controllers
             return View(products);
         }
 
-        
+        #region Must be Used Later
+        //public IActionResult ActiveList()
+        //{
+        //    List<Product> products = _context.Products
+        //        .Include(p => p.ProductImages)
+        //        .Include(p => p.Category)
+        //        .Include(p => p.Brand)
+        //        .Where(p => p.IsDeleted == false).ToList();
+
+        //    return View(products);
+        //}
+        //public IActionResult DeleteList()
+        //{
+        //    List<Product> products = _context.Products
+        //        .Include(p => p.ProductImages)
+        //        .Include(p => p.Category)
+        //        .Include(p => p.Brand)
+        //        .Where(p => p.IsDeleted == true).ToList();
+
+        //    return View(products);
+        //}
+
+        //public IActionResult Undelete(int id)
+        //{
+        //    Product deletedProduct = _context.Products.Find(id);
+        //    if (deletedProduct == null) return NotFound();
+        //    deletedProduct.DeletedAt = null;
+        //    deletedProduct.IsDeleted = false;
+        //    deletedProduct.CreatedAt = DateTime.Now;
+        //    _context.SaveChanges();
+        //    return RedirectToAction("index");
+        //}
+        #endregion
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null) return NotFound();
+
+            Product dbProduct = _context.Products.Include(p=>p.Category).FirstOrDefault(b => b.Id == id);
+
+            if (dbProduct == null) return NotFound();
+
+            ViewBag.Categories = new SelectList(_context.Categories.Where(c => c.Children.Count == 0 || c.ParentId != null).ToList(), "Id", "Name");
+            ViewBag.Brands = new SelectList(_context.Brands.ToList(), "Id", "Name");
+            ViewBag.Tags = new SelectList(_context.Tags.ToList(), "Id", "Name");
+            return View(dbProduct);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Product product)
+        {
+            Product dbProduct = _context.Products.Include(p => p.ProductImages).FirstOrDefault(b => b.Id == product.Id);
+            if (dbProduct == null) return NotFound();
+
+            if (ModelState["Image"] != null)
+            {
+                if (!product.Image.IsImage())
+                {
+                    ModelState.AddModelError("Image", "Image Format Is Wrong");
+                    return View();
+                }
+                if (product.Image.ImageSize(8000))
+                {
+                    ModelState.AddModelError("Image", "Image Is Oversize");
+                    return View();
+                }
+
+
+                string patsh = Path.Combine(_env.WebRootPath, @"assets\images\product", dbProduct.ProductImages.Find(i => i.IsMain == true).ImageUrl).ToString();
+                ImageService.DeleteImage(patsh);
+                dbProduct.ProductImages.Find(i => i.IsMain == true).ImageUrl = "images/product/" + product.Image.SaveImage(_env, @"assets\images\product");
+            }
+
+
+            var existName = _context.Products.FirstOrDefault(x => x.Name.ToLower() == product.Name.ToLower());
+
+            if (existName != null)
+            {
+                if (dbProduct.Name.ToLower() != existName.Name.ToLower())
+                {
+                    ModelState.AddModelError("Name", "Model Is Exsist");
+                    return View("Update");
+                }
+            }
+
+            dbProduct.Name = product.Name;
+            dbProduct.CategoryId = product.CategoryId;
+            dbProduct.BrandId = product.BrandId;
+            dbProduct.TagProducts = product.TagProducts;
+            dbProduct.UptadetAt = DateTime.Now;
+
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            Product dbProduct = _context.Products.Find(id);
+
+            if (dbProduct == null) return NotFound();
+
+            dbProduct.IsDeleted = true;
+            dbProduct.DeletedAt = DateTime.Now;
+            dbProduct.CreatedAt = null;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Categories = new SelectList(_context.Categories.Where(c => c.Children.Count == 0 || c.ParentId != null && c.IsDeleted == false).ToList(), "Id", "Name");
+            ViewBag.Brands = new SelectList(_context.Brands.Where(b => b.IsDeleted == false).ToList(), "Id", "Name");
+            ViewBag.Tags = new SelectList(_context.Tags.ToList(), "Id", "Name");
+            Product product = new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+
+            if (!ModelState.IsValid) return View();
+
+            Product newProduct = new Product
+            {
+                Name = product.Name,
+                BrandId = product.BrandId,
+                CategoryId = product.CategoryId,
+                ProductImages = product.ProductImages,
+                CreatedAt = DateTime.Now,
+                Price = product.Price,
+                StockCount = product.StockCount,
+            };
+            List<ProductImage> productImages = new List<ProductImage>();
+            ProductImage newproductImage = new ProductImage
+            {
+                ImageUrl = "images/" + product.Image.SaveImage(_env, @"assets\images"),
+                ProductId = newProduct.Id,
+                IsMain = true
+            };
+
+            productImages.Add(newproductImage);
+            newProduct.ProductImages = productImages;
+            _context.Products.Add(newProduct);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
