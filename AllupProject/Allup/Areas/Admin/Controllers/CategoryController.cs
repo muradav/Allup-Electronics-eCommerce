87537e69b4b953f53fad0aa0cf895e8bf86f7ddc
muradav <muradav@code.edu.az>
2022-07-30 15:43:39 +0400

    feat: category controller index view created

--- AllupProject/Allup/Areas/Admin/Controllers/CategoryController.cs
+++ AllupProject/Allup/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Allup.DAL;
+using Allup.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Allup.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public CategoryController(AppDbContext context, IWebHostEnvironment env)
         {
-            return View();
+            _context = context;
+            _env = env;
         }
-    }
+		public IActionResult Index()
+		{
+			var categories = _context.Categories.ToList();
+
+			return View(categories);
+		}
+		
+
+	}
 }
